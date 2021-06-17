d55cdef17c8fed3045b227fc1f558b8358ecefdd
Felix de Maneville <felix.maneville@gmail.com>
2021-06-17 10:19:08 +0200

    Project setup

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use bevy::log::{Level, LogSettings};
+use bevy::prelude::*;
+
+#[cfg(feature = "debug")]
+use bevy_inspector_egui::WorldInspectorPlugin;
+
+fn main() {
+    let mut app = App::build();
+    // Window setup
+    app.insert_resource(WindowDescriptor {
+        title: "Mine Sweeper!".to_string(),
+        width: 700.,
+        height: 1000.,
+        ..Default::default()
+    })
+    // Log setup
+    .insert_resource(LogSettings {
+        level: Level::INFO,
+        ..Default::default()
+    })
+    // Board plugin options
+    // Bevy default plugins
+    .add_plugins(DefaultPlugins)
+    // Startup system (cameras)
+    .add_startup_system(setup.system());
+    #[cfg(feature = "debug")]
+    // Debug hierarchy inspector
+    app.add_plugin(WorldInspectorPlugin::new());
+    // Run the app
+    app.run();
+}
+
+fn setup(mut commands: Commands) {
+    // 2D orthographic camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
